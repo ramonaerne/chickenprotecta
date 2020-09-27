d1bff6426325edbf19b639e784c81e891faaf76a
Ramon Aerne <ramon.aerne@noser.com>
2020-09-27 14:41:58 +0200

    updated with swu

--- chickenprotecta-public.ino
+++ chickenprotecta-public.ino
@@ -4,6 +4,7 @@
 #include <Servo.h>
 #include <BlynkSimpleEsp8266.h>
 #include <EEPROM.h>
+#include <ArduinoOTA.h>
 #include "DHT.h"
 
 #define D_OPEN_A  0x0
@@ -13,7 +14,7 @@
 #define D_PREV_A  0x8
 #define S_PREV_A  0xA
 
-#define DELAY_ATTACH_MILLI 1000
+#define DELAY_ATTACH_MILLI 2000
 #define DELAY_DOOR_MILLI 1000
 #define DELAY_SLIDER_MILLI 1000
 
@@ -186,11 +187,60 @@ void setup()
   s_slider.write(init_angle);
 
   dht.begin();
+
+  
+  // Port defaults to 8266
+  // ArduinoOTA.setPort(8266);
+
+  // Hostname defaults to esp8266-[ChipID]
+  // ArduinoOTA.setHostname("myesp8266");
+
+  // No authentication by default
+  // ArduinoOTA.setPassword("admin");
+
+  // Password can be set with it's md5 value as well
+  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else { // U_FS
+      type = "filesystem";
+    }
+
+    // NOTE: if updating FS this would be the place to unmount FS using FS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+  ArduinoOTA.begin();
 }
 
 void loop()
 {
   Blynk.run();
+
+  ArduinoOTA.handle();
 
   digitalWrite(LED_BUILTIN, HIGH);   // Arduino: turn the LED on (HIGH)
                                      // D1 Mini: turns the LED *off*
