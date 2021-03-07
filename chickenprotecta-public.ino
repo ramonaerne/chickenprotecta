3beaae2f2edfd22efaea1ab35878ee4fb135f4a8
Ramon <raemaerne@gmail.com>
2021-03-07 21:04:11 +0100

    working version, in production now

--- chickenprotecta-public.ino
+++ chickenprotecta-public.ino
@@ -15,9 +15,9 @@
 #include <time.h>
 #include <arduino-timer.h>
 
-/* ------------------------------------------------------ 
- *  Defines and constants
- * ------------------------------------------------------ */
+/* ------------------------------------------------------
+   Defines and constants
+  ------------------------------------------------------ */
 
 #define D_OPEN_A  0x0
 #define D_CLOSE_A 0x2
@@ -30,7 +30,7 @@
 #define SUNSET_OFF_A  0xE
 
 #define DELAY_ATTACH_MILLI 2000
-#define DELAY_DOOR_MILLI 2000
+#define DELAY_DOOR_MILLI 5000
 #define DELAY_SLIDER_MILLI 1000
 
 #define SERVO_DOOR_MICRO_MIN 600
@@ -38,11 +38,15 @@
 #define SERVO_MOVEMENT_MAX_MILLI 16000
 
 #define NTP_SYNC_TIME_SEC 10
-#define TIMER_INTERVAL_SEC 15
+#define TIMER_INTERVAL_SEC 60
 
 #define LOC_LONGITUDE 40.0
 #define LOC_LATITUDE 40.0
 #define LOC_TIMEZONE_OFFSET 1
+
+#define PIN_SLIDER D2
+#define PIN_DOOR D1
+
 
 //#define DEBUG 1
 
@@ -55,11 +59,11 @@ char auth[] = "****";
 char ssid[] = "****";
 char pass[] = "****";
 
-/* ------------------------------------------------------ 
- *  Global instances
- * ------------------------------------------------------ */
+/* ------------------------------------------------------
+   Global instances
+  ------------------------------------------------------ */
 Servo s_door, s_slider;
-//DHT dht(D5, DHT22);
+DHT dht(D5, DHT22);
 
 WidgetTerminal terminal(V6);
 
@@ -76,33 +80,33 @@ Timezone CE(CEST, CET);
 bool moving_door = false;
 void move_door(int button);
 
-/* ------------------------------------------------------ 
- *  EEPROM helper functions
- * ------------------------------------------------------ */
+/* ------------------------------------------------------
+   EEPROM helper functions
+  ------------------------------------------------------ */
 
 template <class T> int EEPROM_writeAnything(int ee, const T& value)
 {
-    const byte* p = (const byte*)(const void*)&value;
-    unsigned int i;
-    for (i = 0; i < sizeof(value); i++)
-          EEPROM.write(ee++, *p++);
-    return i;
+  const byte* p = (const byte*)(const void*)&value;
+  unsigned int i;
+  for (i = 0; i < sizeof(value); i++)
+    EEPROM.write(ee++, *p++);
+  return i;
 }
 
 template <class T> int EEPROM_readAnything(int ee, T& value)
 {
-    byte* p = (byte*)(void*)&value;
-    unsigned int i;
-    for (i = 0; i < sizeof(value); i++)
-          *p++ = EEPROM.read(ee++);
-    return i;
+  byte* p = (byte*)(void*)&value;
+  unsigned int i;
+  for (i = 0; i < sizeof(value); i++)
+    *p++ = EEPROM.read(ee++);
+  return i;
 }
 
 int16_t readEEPROM(int address) {
   int16_t val;
   int bytes = EEPROM_readAnything(address, val);
 
-  if(bytes != sizeof(int16_t)) {
+  if (bytes != sizeof(int16_t)) {
     Serial.println("failed to read bytes for integer");
     return -1;
   }
@@ -112,32 +116,32 @@ int16_t readEEPROM(int address) {
 
 void writeEEPROM(int address, int16_t val) {
   int bytes = EEPROM_writeAnything(address, val);
-  if(bytes != sizeof(int16_t)) {
+  if (bytes != sizeof(int16_t)) {
     Serial.println("failed to write bytes for integer");
     return;
   }
   EEPROM.commit();
 }
 
-/* ------------------------------------------------------ 
- *  Time/NTP and Sunset/Sunrise helper functions
- * ------------------------------------------------------ */
- 
-static tm getDateTimeByParams(long time){
-    struct tm *newtime;
-    const time_t tim = time;
-    newtime = localtime(&tim);
-    return *newtime;
+/* ------------------------------------------------------
+   Time/NTP and Sunset/Sunrise helper functions
+  ------------------------------------------------------ */
+
+static tm getDateTimeByParams(long time) {
+  struct tm *newtime;
+  const time_t tim = time;
+  newtime = localtime(&tim);
+  return *newtime;
 }
 
 int getMinutesSinceMidnighteLoc() {
   tm loc;
   loc = getDateTimeByParams(CE.toLocal(now()));
-  Serial.printf("%d %d %d %d:%d\n", loc.tm_year + 1900, loc.tm_mon+1, loc.tm_mday, loc.tm_hour, loc.tm_min);
+  Serial.printf("%d %d %d %d:%d\n", loc.tm_year + 1900, loc.tm_mon + 1, loc.tm_mday, loc.tm_hour, loc.tm_min);
   return loc.tm_hour * 60 + loc.tm_min;
 }
 
-void printDoorUp() 
+void printDoorUp()
 {
   int sunrise = location.sunrise(year(), month(), day(), CE.utcIsDST(now()));
   int sunset = location.sunset(year(), month(), day(), CE.utcIsDST(now()));
@@ -160,7 +164,7 @@ void printDoorUp()
 static bool isDoorUp() {
 
   int minsSinceMidnight = getMinutesSinceMidnighteLoc();
-  
+
   int sunrise = location.sunrise(year(), month(), day(), CE.utcIsDST(now()));
   int sunset = location.sunset(year(), month(), day(), CE.utcIsDST(now()));
 
@@ -176,56 +180,52 @@ bool updateTimeAndDoTask(void *) {
 
   // manual update of clock since setsyncprovider
   // does not work correctly
-  if (timeClient.update()){
-     Serial.println ( "Update clock" );
-     setTime(timeClient.getEpochTime());   
-  } else{
-     Serial.println ( "NTP Update not WORK!!" );
+  if (timeClient.update()) {
+    Serial.println ( "Update clock" );
+    setTime(timeClient.getEpochTime());
+  } else {
+    Serial.println ( "NTP Update not WORK!!" );
   }
 
   bool doorUp = isDoorUp();
   if (!moving_door)
     move_door(doorUp);
-  
+
   return true; // keep timer running
 }
 
-/* ------------------------------------------------------ 
- *  Servo member functions and Blynk macros
- * ------------------------------------------------------ */
+/* ------------------------------------------------------
+   Servo member functions and Blynk macros
+  ------------------------------------------------------ */
 
 int getApproximateServoDelay(int old_pos, int new_pos) {
-  
-  int diff = abs(new_pos-old_pos);
-  int del = diff * SERVO_MOVEMENT_MAX_MILLI / (SERVO_DOOR_MICRO_MAX-SERVO_DOOR_MICRO_MIN);
+
+  int diff = abs(new_pos - old_pos);
+  int del = diff * SERVO_MOVEMENT_MAX_MILLI / (SERVO_DOOR_MICRO_MAX - SERVO_DOOR_MICRO_MIN);
   del += DELAY_DOOR_MILLI;
   printf("delay: %d\n", del);
-  if(del < 0) del = 0;
-  if(del > SERVO_MOVEMENT_MAX_MILLI) del = SERVO_MOVEMENT_MAX_MILLI;
+  if (del < 0) del = 0;
+  if (del > SERVO_MOVEMENT_MAX_MILLI) del = SERVO_MOVEMENT_MAX_MILLI;
 
   return del;
 }
 
 BLYNK_WRITE(V0) // door min
 {
-  s_door.attach(D3); // d1
-  s_slider.attach(D4); // d2
   int data = param.asInt();
- Serial.printf("V0 %d\n", data);
+  Serial.printf("V0 %d\n", data);
   // s_door.writeMicroseconds(data);
- writeEEPROM(D_CLOSE_A, data);
+  writeEEPROM(D_CLOSE_A, data);
   // writeEEPROM(D_PREV_A, data);
 
 }
 
 BLYNK_WRITE(V1) // door max
 {
-  s_door.attach(D3);
-  s_slider.attach(D4);
   int data = param.asInt();
   Serial.printf("V1 %d\n", data);
   //s_door.writeMicroseconds(data);
-   writeEEPROM(D_OPEN_A, data);
+  writeEEPROM(D_OPEN_A, data);
   // writeEEPROM(D_PREV_A, data);
 
 }
@@ -233,12 +233,12 @@ BLYNK_WRITE(V1) // door max
 bool get_door_position()
 {
   int door_milli_up, door_milli_down, door_milli_prev;
-  
+
   door_milli_prev = readEEPROM(D_PREV_A);
   door_milli_up   = readEEPROM(D_OPEN_A);
   door_milli_down = readEEPROM(D_CLOSE_A);
 
-  return abs(door_milli_prev-door_milli_up) < abs(door_milli_prev - door_milli_down);
+  return abs(door_milli_prev - door_milli_up) < abs(door_milli_prev - door_milli_down);
 }
 
 void move_door(int button)
@@ -246,25 +246,25 @@ void move_door(int button)
   int angle, angle_prev;
   int door_milli, door_milli_prev;
 
-  if(get_door_position() == button) {
+  if (get_door_position() == button) {
     Serial.println("skip move_door, already in position");
     return;
   }
 
   moving_door = true;
   Blynk.setProperty(V2, "color", "#000000");
-  
+
   door_milli_prev = readEEPROM(D_PREV_A);
-  s_door.attach(D3);
-  s_slider.attach(D4);
+  s_door.attach(PIN_DOOR);
+  s_slider.attach(PIN_SLIDER);
   delay(DELAY_ATTACH_MILLI);
 
-  if(button == 1) {
+  if (button == 1) {
     // button on = door up
     door_milli = readEEPROM(D_OPEN_A);
     angle =  readEEPROM(S_OPEN_A);
-  
-    // door up: 
+
+    // door up:
     // 0. power up servos (init pos should be the last)
     // 1. (wait for last position to be taken)
     // 2. lift door
@@ -272,9 +272,9 @@ void move_door(int button)
     // 4. move slider
     // 5. wait for slider to finish
     // 6. detach servos (door should come down to slider)
-  
+
     s_door.writeMicroseconds(door_milli);
-  
+
     // wait
     Serial.print("lifting door...");
     terminal.print("lifting door..."); terminal.flush();
@@ -282,19 +282,19 @@ void move_door(int button)
     delay(del);
     Serial.printf("done in %dms\n", del);
     terminal.printf("done in %dms\n", del); terminal.flush();
-  
+
     s_slider.write(angle);
     Serial.print("locking door...");
     terminal.print("locking door..."); terminal.flush();
     delay(DELAY_SLIDER_MILLI);
     Serial.print("done\n");
     terminal.print("done\n"); terminal.flush();
-  
+
   } else {
     // button off = door down
     door_milli = readEEPROM(D_CLOSE_A);
     angle =  readEEPROM(S_CLOSE_A);
-  
+
     // door down
     // 1. power up servos (init pos should be the last)
     // 2. wait for last position to be taken -> this will free the slider
@@ -303,25 +303,25 @@ void move_door(int button)
     // 5. lower door
     // 6. wait for door to finish
     // 7. detach servos (door should come down to bottom)
-  
+
     s_slider.write(angle);
     Serial.print("unlocking door...");
-    terminal.print("unlocking door..."); terminal.flush(); 
+    terminal.print("unlocking door..."); terminal.flush();
     delay(DELAY_SLIDER_MILLI);
-    Serial.print("done\n");  
-    terminal.print("done\n"); terminal.flush(); 
-  
+    Serial.print("done\n");
+    terminal.print("done\n"); terminal.flush();
+
     s_door.writeMicroseconds(door_milli);
     Serial.print("lowering door...");
-    terminal.print("lowering door..."); terminal.flush(); 
+    terminal.print("lowering door..."); terminal.flush();
     int del = getApproximateServoDelay(door_milli_prev, door_milli);
     delay(del);
     Serial.printf("done in %dms\n", del);
     terminal.printf("done in %dms\n", del); terminal.flush();
   }
-  
+
   s_door.detach();
-  s_slider.detach();   
+  s_slider.detach();
   writeEEPROM(D_PREV_A, door_milli);
   writeEEPROM(S_PREV_A, angle);
 
@@ -355,9 +355,9 @@ BLYNK_WRITE(V5) // slider open
 }
 BLYNK_READ(V4)
 {
-  //int t = dht.readTemperature();
-  //int h = dht.readHumidity();
-  //Blynk.virtualWrite(V4, t);
+  int t = dht.readTemperature();
+  int h = dht.readHumidity();
+  Blynk.virtualWrite(V4, t);
 }
 
 
@@ -370,7 +370,7 @@ void helpTerminal()
   terminal.flush();
 }
 
-void setupTerminal() 
+void setupTerminal()
 {
   terminal.clear();
   helpTerminal();
@@ -413,7 +413,7 @@ void updateAllVirtualPinsFromEEPROM()
 {
   Blynk.virtualWrite(V0, readEEPROM(D_CLOSE_A));
   Blynk.virtualWrite(V1, readEEPROM(D_OPEN_A));
-  
+
   Blynk.virtualWrite(V2, get_door_position());
   Blynk.virtualWrite(V3, readEEPROM(S_CLOSE_A));
   Blynk.virtualWrite(V5, readEEPROM(S_OPEN_A));
@@ -422,10 +422,10 @@ void updateAllVirtualPinsFromEEPROM()
   Blynk.virtualWrite(V8, readEEPROM(SUNSET_OFF_A));
 }
 
-/* ------------------------------------------------------ 
- *  Main arduino setup and loop functions
- * ------------------------------------------------------ */
- 
+/* ------------------------------------------------------
+   Main arduino setup and loop functions
+  ------------------------------------------------------ */
+
 void setup()
 {
   // Debug console
@@ -436,7 +436,7 @@ void setup()
 
   // Setup wifi and blynk
   WiFi.begin(ssid, pass);
- 
+
   while ( WiFi.status() != WL_CONNECTED ) {
     delay ( 500 );
     Serial.print ( "." );
@@ -456,26 +456,31 @@ void setup()
   s_door.writeMicroseconds(init_milli);
   s_slider.write(init_angle);
 
+  //s_door.writeMicroseconds(readEEPROM(D_OPEN_A));
+  //s_slider.write(90);
+  //s_door.writeMicroseconds(readEEPROM(D_CLOSE_A));
+  //s_slider.write(readEEPROM(S_CLOSE_A));
+
   // setup temp sensor
-  //dht.begin();
+  dht.begin();
 
   // set initial time and timer
-  timeClient.begin(); 
-  setTime(timeClient.getEpochTime()); 
+  timeClient.begin();
+  setTime(timeClient.getEpochTime());
   timer.every(TIMER_INTERVAL_SEC * 1000, updateTimeAndDoTask);
 
   // setup OTA update
   // TODO: enable security
-  
+
   // Port defaults to 8266
   // ArduinoOTA.setPort(8266);
-  
+
   // Hostname defaults to esp8266-[ChipID]
   // ArduinoOTA.setHostname("myesp8266");
-  
+
   // No authentication by default
   // ArduinoOTA.setPassword("admin");
-  
+
   // Password can be set with it's md5 value as well
   // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
   // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
@@ -487,7 +492,7 @@ void setup()
     } else { // U_FS
       type = "filesystem";
     }
-  
+
     // NOTE: if updating FS this would be the place to unmount FS using FS.end()
     Serial.println("Start updating " + type);
   });
